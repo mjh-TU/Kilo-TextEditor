78acb378a072d4a83977963b1ce2c629aeacae0b
mjh-TU <mjh8018@utulsa.edu>
2025-02-12 20:03:01 -0600

    3/4th through rawInputAndOutput

--- kilo.c
+++ kilo.c
@@ -3,14 +3,38 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+
+/*** defines ***/
+#define KILO_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 /*** data ***/
 struct termios orig_termios;
 
+struct editorConfig {
+    int cx, cy;
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 // Die function to print an error message and exit program
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -19,7 +43,7 @@ void die(const char *s) {
 void disableRawMode() {
     // Set terminal attributes back to normal with orig_termios
     // You may notice that leftover input is no longer fed back to the shell. TCSAFLUSH below takes care of that
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
@@ -27,12 +51,12 @@ void disableRawMode() {
 void enableRawMode() {
     
     // Gather terminal attributes. orig_termios is the original terminal attributes.
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     // Register out disableRawMode to be called automatically when the program exits
     atexit(disableRawMode);
 
     
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     // Modify the terminal attributes. Echo is a bitflag, we use '~' to get the opposite bit then bitwise-AND this value with the flags field forcing the fourth bit to become `0` and every other bit to change back to its original value.
     // ICANON flag reads input byte-by-byte not line-by-line
@@ -59,26 +83,208 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+
+int editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[' ) {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+
+        return '\x1b';
+    }
+    else {
+        return c;
+    }
+}
+
+
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+    
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+  }
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        // On success ioctl() will place number of columns wide and number of rows high the terminal is given to winsize struct.
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+
+/*** append buffer ***/
+
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+// Instead of using our write function multiple times append data to dynamic string and then finally write output to terminal. Creating our own dynamic string using the heap.
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+  }
+  void abFree(struct abuf *ab) {
+    free(ab->b);
+  }
+
+
+/*** output ***/
+
+// Draws a ~ in each row, which means that row is not part of the file and can't contain any text
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+
+        if (y == E.screenrows / 3) {
+            // Create welcome message
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+
+            // Center message
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } 
+        else {
+            abAppend(ab, "~", 1);
+        }
+
+        abAppend(ab, "\x1b[K", 3);
+
+        // If last line don't create new line
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
+    }
+}
+
+
+void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+
+    // Hide cursor
+    abAppend(&ab, "\x1b[?25l", 6);
+    
+    // Reposition cursor at top left of screen
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    // Move cursor to the position stored in E.cx and E.cy
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    // Show cursor
+    abAppend(&ab, "\x1b[?25h", 6);
+
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+
+/*** input ***/
+
+void editorMoveCursor(int key) {
+    switch (key) {
+      case ARROW_LEFT:
+        E.cx--;
+        break;
+      case ARROW_RIGHT:
+        E.cx++;
+        break;
+      case ARROW_UP:
+        E.cy--;
+        break;
+      case ARROW_DOWN:
+        E.cy++;
+        break;
+    }
+}
+
+
+void editorProcessKeypress() {
+    int c = editorReadKey();
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+    }
+}
+
+
 /*** init ***/
+
+void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     // Disbale the echo feature
     enableRawMode();
+    initEditor();
 
     // Read STDIN and save to char c variable. If variable is q then quit. Runs infinitely.
     while (1) {
-        char c = '\0';
-        read(STDIN_FILENO, &c, 1);
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        // If character c is a control character. Character that wont't print to screen
-        if (iscntrl(c)) {
-            // Format the byte as a decimal number (ascii).
-            printf("%d\r\n", c);
-        }
-        else {
-            // Format byte as a decimal number (ascii) and %c writes byte as a character
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
 
     // Default return code for C which represents a successful run
